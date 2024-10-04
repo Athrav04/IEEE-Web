f4596f54f422281d760d47741360d23b1608c91a
CK-Check <chetandell3000@gmail.com>
2024-10-05 01:20:30 +0530

    Added the Navbar and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from "./components/NavBar";
 import Home from './components/Home';
+import Footer from './components/Footer';
 import './App.css'
 import { Router } from 'express';
 
 function App() {
   return(
   <>
-    <NavBar/>
-    <Home/>
+  <div>
+    <div className='relative'>
+      <div className='fixed top-0 z-10'>
+        <NavBar/>
+      </div>
+        <Home/>
+    </div>
+  </div>
+  <Footer/>
   </>
   )
   }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+const Footer = ()=>{
+    return(
+        <>
+        <footer className="grid grid-cols-3 place-items-center bg-slate-200 text-slate-500">
+            <div className="m-5">
+                <ul>
+                    <p className="font-semibold">Quick Links</p>
+                    <div className="my-3">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/events">Events</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </div>
+                </ul>
+            </div>
+            <div className="m-5">
+                <ul>
+                <p className="font-semibold">Connect With Us</p>
+                <div className="my-3">
+                    <li><a>Facebook</a></li>
+                    <li><a>Twitter</a></li>
+                    <li><a>Instagram</a></li>
+                    <li><a>LinkedIn</a></li>
+                </div>
+                </ul>
+            </div>
+            <div className="m-5">
+                <ul>
+                <p className="font-semibold">Legal</p>
+                <div className="my-3">
+                    <li><a>Terms of Use</a></li>
+                    <li><a>Privacy Policy</a></li>
+                    <li><a>Cookies Policy</a></li>
+                    <li><a>Sitemap</a></li>
+                </div>
+                </ul>
+            </div>
+        </footer>
+        </>
+    )
+};
+
+
+export default Footer;
