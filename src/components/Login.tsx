d1b215992a905de183a57f9cb6218834d025b4d6
Athrav04 <atharvakhandagale04@gmail.com>
2024-10-08 02:43:33 +0530

    Done login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,17 +4,20 @@ import { UserForm } from './config'
 const Login = () => {
 
   return (
-    <div className='h-screen grid grid-cols-4'>
-     <div className='flex justify-center items-center  col-span-4 md:col-span-2 md:flex md:justify-center md:items-center'>
-        <UserForm />
-     </div>
-    <img
-    src='/atmosphere.jpg'
-    alt='loing-bg-image'
-    className='h-screen w-full object-cover col-span-2'>
-    </img>
+    <section className='grid grid-cols-4 '>
+      <div className=' absolute '><img src='./ieee_logo.png' alt='ieee_logo' className='object-cover top-0 left-0' height={200} width={200} /></div>
+      <div className='h-screen flex flex-col justify-center items-center col-span-2'>
+        <div className='flex flex-col items-start'>
+          <h1 className='mb-4 text-4xl font-bold'>Welcome Back!</h1>
+          <UserForm />
+        </div>
 
-    </div>
+      </div>
+      <div className='h-screen col-span-2'>
+          <img src='./atmosphere.jpg'alt='login-bg-image' className='h-screen w-full object-cover rounded-l-3xl'/>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -23,19 +23,26 @@ export default function UserForm() {
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-start gap-3 w-full">
       {/* register your input into the hook by invoking the "register" function */}
-      <input placeholder="11324" {...register("userId")} />
-      <input placeholder="John1" {...register("userName")} />
-
-
+      <div className="w-full">
+        <p className="text-sm text-gray-500 text-bold mb-1">UserName</p>
+       <input placeholder="John1" {...register("userName",{required:true})} className={`rounded-md pl-2 p-[4px] w-full bg-gray-100 ${errors.userName ? 'border-red-500' : 'border-[1px]'}`}/>
+      </div>
+    
       {/* include validation with required or other standard HTML validation rules */}
-      <input placeholder="password" {...register("password", { required: true , minLength : 6})} />
+      <div className="w-full">
+        <p className="text-sm text-gray-500 text-bold mb-1">password</p>
+        <input placeholder="password" {...register("password", { required: true , minLength : 6})} className={`border-[1px] rounded-md pl-2 p-[4px] w-full bg-gray-100 ${errors.password ? 'border-2 border-red-500' : 'border-[1px]'}`} />
+      </div>
+      
       {/* errors will return when field validation fails  */}
-      {errors.password && <span>This is a reuqired field</span>}
+      {/* {errors.password && <span className="text-sm text-red-500"> is a reuqired field</span>} */}
 
 
-      <input type="submit" />
+      <button type="submit" className=" cursor-pointer place-self-center w-full mt-2 p-1 bg-gradient-to-r from-sky-300 to-sky-500 rounded-md text-center items-center">
+        Login
+      </button>
     </form>
   )
 }
