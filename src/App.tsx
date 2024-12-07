73d4022bfb82e5a84bf4bb5adb24d0b31ec1c8b4
CK-Check <chetandell3000@gmail.com>
2024-12-07 13:48:26 +0530

    Committee page template is ready.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import viteLogo from '/vite.svg'
 import NavBar from "./components/NavBar";
 import Home from './components/Home';
 import Footer from './components/Footer';
-import './App.css'
-import { Router } from 'express';
-import Committee from './components/Committee';
+import './App.css';
 
 function App() {
   return(
   <>
   <div>
-    <div className='relative'>
-      <div className='fixed top-0 z-10'>
-        <NavBar/>
+      <div className='relative'>
+        <div className='fixed top-0 z-10'>
+          <NavBar/>
+        </div>
+        <Home />
       </div>
-        {/* <Home/> */}
-        <Committee/>
-    </div>
   </div>
   <Footer/>
   </>
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Cards = () => {
     return(
-        <div className="flex justify-center">
+        <div className="flex justify-center m-7 rounded-xl">
             <div className="relative">
-            <div className="absolute rounded-xl bottom-0 w-full text-center text-white bg-gradient-to-b from-transparent to-black to-65%">
+            <div className="absolute rounded-xl bottom-0 w-full text-center text-white bg-gradient-to-b from-transparent to-black to-65% text-base md:max-lg:text-xs">
                 <p>Chetan G Kotangale</p>
                 Web Dev Joint Head 
             </div>
-            <img className="object-contain h-72 rounded-xl" src="/man.jpg" alt="/man.jpg"/>
+            <img className="object-contain rounded-xl" src="/man.jpg" alt="/man.jpg"/>
             </div>
         </div>
     )
