f5899f0c984c8cf79bb1625e239321d068daa71f
CK-Check <chetandell3000@gmail.com>
2025-01-13 18:30:16 +0530

    Rebuild the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,37 @@
-const Footer = ()=>{
+const Footer = (props: { fb: string, ig: string, x: string, linkedIn: string })=>{
     return(
         <>
-        <footer className="grid grid-cols-3 place-items-center bg-[#08090a] border-t-[0.5px] border-[#373737] text-white font-thin">
-            <div className="m-5">
+        <footer className="grid md:grid-cols-3 md:place-items-baseline bg-[#08090a] border-t-[0.5px] border-[#373737] text-white font-thin md:mx-9">
+            <div className="m-7">
                 <ul>
-                    <p className="font-normal">Quick Links</p>
+                    <p className="font-semibold md:my-7">Quick Links</p>
                     <div className="my-3">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/events">Events</a></li>
-                        <li><a href="/contact">Contact</a></li>
+                        <li className="md:my-1"><a href="/">Home</a></li>
+                        <li className="md:my-1"><a href="/about">About</a></li>
+                        <li className="md:my-1"><a href="/events">Events</a></li>
+                        <li className="md:my-1"><a href="/contact">Contact</a></li>
                     </div>
                 </ul>
             </div>
-            <div className="m-5">
+            <div className="mx-7">
                 <ul>
-                <p className="font-semibold">Connect With Us</p>
+                <p className="font-semibold md:my-7">Legal</p>
                 <div className="my-3">
-                    <li><a>Facebook</a></li>
-                    <li><a>Twitter</a></li>
-                    <li><a>Instagram</a></li>
-                    <li><a>LinkedIn</a></li>
+                    <li className="md:my-1"><a>Terms of Use</a></li>
+                    <li className="md:my-1"><a>Privacy Policy</a></li>
+                    <li className="md:my-1"><a>Cookies Policy</a></li>
+                    <li className="md:my-1"><a>Sitemap</a></li>
                 </div>
                 </ul>
             </div>
-            <div className="m-5">
+            <div className="m-5 md:w-44">
                 <ul>
-                <p className="font-semibold">Legal</p>
+                <p className="font-semibold">Connect With Us</p>
                 <div className="my-3">
-                    <li><a>Terms of Use</a></li>
-                    <li><a>Privacy Policy</a></li>
-                    <li><a>Cookies Policy</a></li>
-                    <li><a>Sitemap</a></li>
+                    <li className="bg-[#ffffff] rounded flex my-3"><a className="text-black font-normal m-1.5" href={props.fb}><i className="fa-brands fa-facebook-f mx-2"></i>FaceBook</a></li>
+                    <li className="bg-[#ffffff] rounded flex my-3"><a className="text-black font-normal m-1.5" href={props.ig}><i className="fa-brands fa-instagram mx-2"></i>Instagram</a></li>
+                    <li className="bg-[#ffffff] rounded flex my-3"><a className="text-black font-normal m-1.5" href={props.x}><i className="fa-brands fa-x-twitter mx-2"></i>Twitter/ X</a></li>
+                    <li className="bg-[#ffffff] rounded flex my-3"><a className="text-black font-normal m-1.5" href={props.linkedIn}><i className="fa-brands fa-linkedin-in mx-2"></i>LinkedIn</a></li>
                 </div>
                 </ul>
             </div>
